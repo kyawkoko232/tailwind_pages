2a861418093fb574b73d65139fe53df841bae1a5
kyawkoko232 <kyawkoko9m@gmail.com>
2023-07-12 21:45:32 +0630

    color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,37 +19,58 @@ export default {
       spacing: {
         'mobile': '1rem',
       },
-      fontFamily:{
-        'roboto':['Roboto','Padauk'],
-        'poppins': ['Poppins','Padauk']
+      fontFamily: {
+        'roboto': ['Roboto', 'Padauk'],
+        'poppins': ['Poppins', 'Padauk']
+      },
+      fontSize: {
+      },
+      colors: {
+        black: {
+          100: "#d1d2d5",
+          200: "#a3a6ab",
+          300: "#767980",
+          400: "#484d56",
+          500: "#1a202c",
+          600: "#151a23",
+          700: "#10131a",
+          800: "#0a0d12",
+          900: "#050609"
         },
-        fontSize: {
+        indigo: {
+          100: "#cfe2ff",
+          200: "#9ec5fe",
+          300: "#6ea8fe",
+          400: "#3d8bfd",
+          500: "#0d6efd",
+          600: "#0a58ca",
+          700: "#084298",
+          800: "#052c65",
+          900: "#031633"
         },
-        colors: {
-          'primary': '#0d6efd',
-          'secondary': '#6c757d',
-          'success': '#198754',
-          'danger': '#dc3545',
-          'warning': '#ffc107',
-          'info': '#0dcaf0',
-          'light': '#f8f9fa',
-          'dark': '#343a40',
-          'bluesoft': 'var( --picton-blue)',
-          dark: {
-            primary: '#1A202C',
-            secondary: '#2D3748',
-            tertiary: '#4A5568',
-          }
+        'secondary': '#6c757d',
+        'success': '#198754',
+        'danger': '#dc3545',
+        'warning': '#ffc107',
+        'info': '#0dcaf0',
+        'light': '#f8f9fa',
+        'dark': '#343a40',
+        dark: {
+          primary: '#1A202C',
+          secondary: '#27374D',
+          tertiary: '#526D82',
+          pure: '#000000',
         }
+      }
     },
-    screens:{
+    screens: {
       'sm': '576px',
       'md': '768px',
       'lg': '992px',
       'xl': '1200px',
       'xxl': '1400px'
     },
-   
+
   },
   plugins: [],
 }
