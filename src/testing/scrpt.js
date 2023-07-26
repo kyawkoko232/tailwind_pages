4d81fd37f3b796d2ab09985bc28ced4362caae41
kyawkoko232 <kyawkoko9m@gmail.com>
2023-07-26 10:21:59 +0630

    fail ham

---
+++ src/testing/scrpt.js
@@ -0,0 +1,14 @@
+import './style.css';
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', ()=>{
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+})
